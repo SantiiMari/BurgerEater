e47d750bfc6158c0e9b835996cd69b6432ef9626
SantiimariSantiimarigit configgit config --global user.name :Santiimari <MaryF.Santibanez@gmail.com>
2019-03-22 22:06:33 -0500

    styling

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
         host: "localhost",
         user: "root",
-        password: "",
+        password: "Diddy666",
         database: "burgers_db"
     });
 };
